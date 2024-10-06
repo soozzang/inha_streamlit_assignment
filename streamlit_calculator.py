77b97f3a0fe3446468bc5911768827ffcdd4bc44
한수현 Soohyun Han <devsoo511@gmail.com>
2024-10-06 16:31:49 +0900

    Update streamlit_calculator.py

--- streamlit_calculator.py
+++ streamlit_calculator.py
@@ -12,7 +12,7 @@ x, y, z = sp.symbols('x y z')
 
 if st.button("계산"):
     try:
-        result = sp.sympify(expression)
+        result = sp.sympify(expression).evalf()
         st.success(f"결과 값: {result}")
     except Exception as e:
         st.error(f"Error: {e}")
